e07cfebc4afa8db15f99618e6c54bb2b01d04cf6
shitsurei <lfgewj21345@163.com>
2019-06-23 19:39:23 +0800

    优化了基数排序的时间复杂度，同时保持空间复杂度的规模不变

--- src/foundation/RadixSort2.java
+++ src/foundation/RadixSort2.java
@@ -1,9 +1,9 @@
 package foundation;
 
 /**
- * 基数排序
+ * 基数排序:改进版，使用指向桶底的结点数组节省遍历链表的时间
  */
-public class RadixSort {
+public class RadixSort2 {
     public static void main(String[] args) {
         int[] arr = new int[]{996, 354, 227, 56, 12, 335, 443, 2, 8, 2009};
         radix(arr);
@@ -23,6 +23,20 @@ public class RadixSort {
         }
     }
 
+    public static class Buckets {
+        Elem[] heads;
+        Elem[] tails;
+
+        public Buckets(int system){
+            this.heads = new Elem[system];
+            this.tails = new Elem[system];
+            for (int i = 0; i < heads.length; i++) {
+                heads[i] = new Elem(Integer.MIN_VALUE, null);
+                tails[i] = heads[i];
+            }
+        }
+    }
+
     /**
      * 基数排序主函数
      *
@@ -32,19 +46,18 @@ public class RadixSort {
         //1.求最大值元素的位数
         int maxIndex = maxIndex(arr);
         //2.准备桶，初始化
-        Elem[] buckets = new Elem[10];
-        initBuckets(buckets);
+        Buckets buckets = new Buckets(10);
         //3.按位循环，由低到高
         for (int i = 0; i < maxIndex; i++) {
             //4.遍历序列
             for (int j = 0; j < arr.length; j++) {
                 //5.元素入桶（注意找桶的逻辑）
-                insertBucket(arr[j], buckets[arr[j] / (int) Math.pow(10, i) % 10]);
+                insertBucket(arr[j], buckets, arr[j] / (int) Math.pow(10, i) % 10 );
             }
             //6.生成新的序列
-            generateNewArray(arr, buckets);
+            generateNewArray(arr, buckets.heads);
             //7.清空桶
-            cleanBucket(buckets);
+            cleanBucket(buckets.heads,buckets.tails);
         }
 
     }
@@ -70,11 +83,13 @@ public class RadixSort {
     /**
      * 清空所有桶
      *
-     * @param buckets
+     * @param heads
      */
-    public static void cleanBucket(Elem[] buckets) {
-        for (int i = 0; i < buckets.length; i++) {
-            buckets[i].next = null;
+    public static void cleanBucket(Elem[] heads, Elem[] tail) {
+        for (int i = 0; i < heads.length; i++) {
+            heads[i].next = null;
+            tail[i] = heads[i];
+
         }
     }
 
@@ -82,13 +97,12 @@ public class RadixSort {
      * 元素入桶
      *
      * @param value
-     * @param bucket
+     * @param buckets
+     * @param index
      */
-    public static void insertBucket(int value, Elem bucket) {
-        while (bucket.next != null) {
-            bucket = bucket.next;
-        }
-        bucket.next = new Elem(value, null);
+    public static void insertBucket(int value, Buckets buckets, int index) {
+        buckets.tails[index].next = new Elem(value, null);
+        buckets.tails[index] = buckets.tails[index].next;
     }
 
     /**
@@ -113,14 +127,4 @@ public class RadixSort {
         return maxIndex;
     }
 
-    /**
-     * 初始化各个桶的头结点
-     *
-     * @param buckets
-     */
-    public static void initBuckets(Elem[] buckets) {
-        for (int i = 0; i < buckets.length; i++) {
-            buckets[i] = new Elem(Integer.MIN_VALUE, null);
-        }
-    }
 }
